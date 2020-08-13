5c5f0db8a963784687e9be94346724992227fe30
Brandon Trabucco <brandon@btrabucco.com>
2020-08-12 18:33:12 -0700

    refactor for public release

--- forward_model/forward_ensemble/__init__.py
+++ forward_model/forward_ensemble/__init__.py
@@ -1,14 +1,14 @@
 from forward_model.data import StaticGraphTask
 from forward_model.logger import Logger
-from forward_model.ensemble.trainers import Ensemble
-from forward_model.ensemble.nets import ForwardModel
+from forward_model.forward_ensemble.trainers import Ensemble
+from forward_model.forward_ensemble.nets import ForwardModel
 import tensorflow.keras.layers as tfkl
 import tensorflow as tf
 import numpy as np
 import os
 
 
-def ensemble(config):
+def forward_ensemble(config):
     """Train a forward model and perform model based optimization
     using a bootstrap ensemble
 
@@ -96,81 +96,3 @@ def ensemble(config):
         config['logging_dir'], 'score.npy'), best_score)
     np.save(os.path.join(
         config['logging_dir'], 'design.npy'), best_design)
-
-
-def second_model_predictions(config):
-    """Train a forward model and perform model based optimization
-    using a conservative objective function
-
-    Args:
-
-    config: dict
-        a dictionary of hyper parameters such as the learning rate
-    """
-
-    # create the training task and logger
-    task = StaticGraphTask(config['task'], **config['task_kwargs'])
-    train_data, validate_data = task.build(bootstraps=2,
-                                           batch_size=config['batch_size'],
-                                           val_size=config['val_size'])
-    logger = Logger(config['logging_dir'])
-
-    # make several keras neural networks with two hidden layers
-    forward_models = [ForwardModel(
-        task.input_shape,
-        hidden=config['hidden_size'],
-        initial_max_std=config['initial_max_std'],
-        initial_min_std=config['initial_min_std'],
-        act=tfkl.ReLU) for b in range(2)]
-
-    # create a trainer for a forward model with a conservative objective
-    trainer = Ensemble(
-        forward_models,
-        forward_model_optim=tf.keras.optimizers.Adam,
-        forward_model_lr=config['forward_model_lr'])
-
-    # create a manager for saving algorithms state to the disk
-    manager = tf.train.CheckpointManager(
-        tf.train.Checkpoint(**trainer.get_saveables()),
-        os.path.join(config['logging_dir'], 'ckpt'), 1)
-
-    # train the model for an additional number of epochs
-    manager.restore_or_initialize()
-    trainer.launch(train_data, validate_data, logger, config['epochs'])
-    manager.save()
-
-    # select the top k initial designs from the dataset
-    indices = tf.math.top_k(task.y[:, 0], k=config['solver_samples'])[1]
-
-    # evaluate the initial design using the oracle and the forward model
-    solution = tf.gather(task.x, indices, axis=0)
-    score = task.score(solution)
-    prediction0 = forward_models[0].get_distribution(solution).mean()
-    prediction1 = forward_models[1].get_distribution(solution).mean()
-
-    # record the prediction and score to the logger
-    logger.record("score", score, 0)
-    logger.record("model_0/prediction", prediction0, 0)
-    logger.record("model_1/prediction", prediction1, 0)
-
-    # perform gradient ascent on the score through the forward model
-    for i in range(1, config['solver_steps'] + 1):
-
-        # back propagate through the forward model
-        with tf.GradientTape() as tape:
-            tape.watch(solution)
-            score = forward_models[0].get_distribution(solution).mean()
-        grads = tape.gradient(score, solution)
-        solution = solution + config['solver_lr'] * grads
-
-        # evaluate the design using the oracle and the forward model
-        gradient_norm = tf.linalg.norm(grads, axis=1)
-        score = task.score(solution)
-        prediction0 = forward_models[0].get_distribution(solution).mean()
-        prediction1 = forward_models[1].get_distribution(solution).mean()
-
-        # record the prediction and score to the logger
-        logger.record("gradient_norm", gradient_norm, i)
-        logger.record("score", score, i)
-        logger.record("model_0/prediction", prediction0, i)
-        logger.record("model_1/prediction", prediction1, i)
