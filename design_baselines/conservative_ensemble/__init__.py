55b2d1f1573ec486c70207921556c88428760823
Brandon Trabucco <brandon@btrabucco.com>
2020-08-19 02:44:48 -0700

    update conservative algorithm

--- design_baselines/conservative_ensemble/__init__.py
+++ design_baselines/conservative_ensemble/__init__.py
@@ -95,115 +95,6 @@ def conservative_ensemble(config):
     # evaluate the starting point
     solution = tf.math.softmax(x) if config['is_discrete'] else x
     score = task.score(solution)
-    prediction = trainer.get_distribution(solution).mean()
-
-    # record the prediction and score to the logger
-    logger.record("score", task.denormalize_y(score), 0)
-    logger.record("prediction", task.denormalize_y(prediction), 0)
-
-    # and keep track of the best design sampled so far
-    best_solution = None
-    best_score = None
-
-    # perform gradient ascent on the score through the forward model
-    for i in range(1, config['solver_steps'] + 1):
-        # back propagate through the forward model
-        with tf.GradientTape() as tape:
-            tape.watch(x)
-            solution = tf.math.softmax(x) if config['is_discrete'] else x
-            score = trainer.get_distribution(solution).mean()
-        grads = tape.gradient(score, x)
-
-        # use the conservative optimizer to update the solution
-        x = x + config['solver_lr'] * grads
-        solution = tf.math.softmax(x) if config['is_discrete'] else x
-
-        # evaluate the design using the oracle and the forward model
-        score = task.score(solution)
-        prediction = trainer.get_distribution(solution).mean()
-        gradient_norm = tf.linalg.norm(
-            tf.reshape(grads, [-1, task.input_size]), axis=-1)
-
-        # record the prediction and score to the logger
-        logger.record("score", task.denormalize_y(score), i)
-        logger.record("prediction", task.denormalize_y(prediction), i)
-        logger.record("gradient_norm", gradient_norm, i)
-
-        # update the best design every iteration
-        idx = np.argmax(score)
-        if best_solution is None or score[idx] > best_score:
-            best_score = score[idx]
-            best_solution = solution[idx]
-
-    # save the best design to the disk
-    np.save(os.path.join(
-        config['logging_dir'], 'score.npy'), best_score)
-    np.save(os.path.join(
-        config['logging_dir'], 'solution.npy'), best_solution)
-
-
-def conservative_ensemble_predictions(config):
-    """Train a forward model and perform model based optimization
-    using a conservative objective function
-
-    Args:
-
-    config: dict
-        a dictionary of hyper parameters such as the learning rate
-    """
-
-    # create the training task and logger
-    logger = Logger(config['logging_dir'])
-    task = StaticGraphTask(
-        config['task'],
-        normalize_x=not config['is_discrete'],
-        normalize_y=True,
-        **config['task_kwargs'])
-    train_data, validate_data = task.build(
-        bootstraps=len(config['activations']),
-        batch_size=config['batch_size'],
-        val_size=config['val_size'])
-
-    # make several keras neural networks with different architectures
-    forward_models = [ForwardModel(
-        task.input_shape,
-        activations=activations,
-        hidden=config['hidden_size'],
-        initial_max_std=config['initial_max_std'],
-        initial_min_std=config['initial_min_std'])
-        for activations in config['activations']]
-
-    # create a trainer for a forward model with a conservative objective
-    trainer = ConservativeEnsemble(
-        forward_models,
-        forward_model_optim=tf.keras.optimizers.Adam,
-        forward_model_lr=config['forward_model_lr'],
-        target_conservative_gap=config['target_conservative_gap'],
-        initial_alpha=config['initial_alpha'],
-        alpha_optim=tf.keras.optimizers.Adam,
-        alpha_lr=config['alpha_lr'],
-        perturbation_lr=config['perturbation_lr'],
-        perturbation_steps=config['perturbation_steps'],
-        is_discrete=config['is_discrete'])
-
-    # create a manager for saving algorithms state to the disk
-    manager = tf.train.CheckpointManager(
-        tf.train.Checkpoint(**trainer.get_saveables()),
-        os.path.join(config['logging_dir'], 'ckpt'), 1)
-
-    # train the model for an additional number of epochs
-    manager.restore_or_initialize()
-    trainer.launch(train_data, validate_data, logger, config['epochs'])
-    manager.save()
-
-    # select the top k initial designs from the dataset
-    indices = tf.math.top_k(task.y[:, 0], k=config['solver_samples'])[1]
-    x = tf.gather(task.x, indices, axis=0)
-    x = tf.math.log(x) if config['is_discrete'] else x
-
-    # evaluate the starting point
-    solution = tf.math.softmax(x) if config['is_discrete'] else x
-    score = task.score(solution)
     preds = [fm.get_distribution(
         solution).mean() for fm in forward_models]
 
@@ -217,6 +108,10 @@ def conservative_ensemble_predictions(config):
         if n > 0:
             logger.record(f"rank_corr/0_to_{n}",
                           spearman(preds[0][:, 0], prediction_i[:, 0]), 0)
+
+    # and keep track of the best design sampled so far
+    best_solution = None
+    best_score = None
 
     # perform gradient ascent on the score through the forward model
     for i in range(1, config['solver_steps'] + 1):
@@ -252,3 +147,15 @@ def conservative_ensemble_predictions(config):
                               spearman(preds[0][:, 0], prediction_i[:, 0]), i)
                 logger.record(f"grad_corr/0_to_{n}", tfp.stats.correlation(
                     grads[0], grads[n], sample_axis=0, event_axis=None), i)
+
+        # update the best design every iteration
+        idx = np.argmax(score)
+        if best_solution is None or score[idx] > best_score:
+            best_score = score[idx]
+            best_solution = solution[idx]
+
+    # save the best design to the disk
+    np.save(os.path.join(
+        config['logging_dir'], 'score.npy'), best_score)
+    np.save(os.path.join(
+        config['logging_dir'], 'solution.npy'), best_solution)
