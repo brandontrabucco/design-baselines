3c8b5a6387b2208c12752e1c7b8cfdf8348312b0
Brandon Trabucco <brandon@btrabucco.com>
2020-09-03 17:14:56 -0700

    add parameters for cbas on molecule task

--- design_baselines/cli.py
+++ design_baselines/cli.py
@@ -618,7 +618,64 @@ def vary_architecture_superconductor(local_dir, cpus, gpus, num_parallel, num_sa
 
 
 @cli.command()
-@click.option('--local-dir', type=str, default='cbas')
+@click.option('--local-dir', type=str, default='cbas-molecule')
+@click.option('--cpus', type=int, default=24)
+@click.option('--gpus', type=int, default=1)
+@click.option('--num-parallel', type=int, default=1)
+@click.option('--num-samples', type=int, default=1)
+def cbas_molecule(local_dir, cpus, gpus, num_parallel, num_samples):
+    """Train a forward model using various regularization methods and
+    solve a model-based optimization problem
+
+    Args:
+
+    local_dir: str
+        the path where model weights and tf events wil be saved
+    cpus: int
+        the number of cpu cores on the host machine to use
+    gpus: int
+        the number of gpu nodes on the host machine to use
+    num_parallel: int
+        the number of processes to run at once
+    num_samples: int
+        the number of samples to take per configuration
+    """
+
+    from design_baselines.cbas import condition_by_adaptive_sampling
+    ray.init(num_cpus=cpus,
+             num_gpus=gpus,
+             temp_dir=os.path.expanduser('~/tmp'))
+    tune.run(condition_by_adaptive_sampling, config={
+        "logging_dir": "data",
+        "is_discrete": True,
+        "task": "MoleculeActivity600885-v0",
+        "task_kwargs": {},
+        "bootstraps": 5,
+        "val_size": 200,
+        "ensemble_batch_size": 128,
+        "vae_batch_size": 32,
+        "hidden_size": 256,
+        "initial_max_std": 1.5,
+        "initial_min_std": 0.5,
+        "ensemble_lr": 0.001,
+        "ensemble_epochs": 100,
+        "latent_size": 32,
+        "vae_lr": 0.001,
+        "vae_beta": 5.0,
+        "offline_epochs": 100,
+        "online_batches": 50,
+        "online_epochs": 10,
+        "iterations": 50,
+        "percentile": 80.0,
+        "solver_samples": 128},
+        num_samples=num_samples,
+        local_dir=local_dir,
+        resources_per_trial={'cpu': cpus // num_parallel,
+                             'gpu': gpus / num_parallel - 0.01})
+
+
+@cli.command()
+@click.option('--local-dir', type=str, default='cbas-gfp')
 @click.option('--cpus', type=int, default=24)
 @click.option('--gpus', type=int, default=1)
 @click.option('--num-parallel', type=int, default=1)
@@ -646,7 +703,7 @@ def cbas_gfp(local_dir, cpus, gpus, num_parallel, num_samples):
              num_gpus=gpus,
              temp_dir=os.path.expanduser('~/tmp'))
     tune.run(condition_by_adaptive_sampling, config={
-        "logging_dir": "gfp",
+        "logging_dir": "data",
         "is_discrete": True,
         "task": "GFP-v0",
         "task_kwargs": {'seed': tune.randint(1000)},
@@ -655,8 +712,8 @@ def cbas_gfp(local_dir, cpus, gpus, num_parallel, num_samples):
         "ensemble_batch_size": 128,
         "vae_batch_size": 10,
         "hidden_size": 50,
-        "initial_max_std": 1.5,
-        "initial_min_std": 0.5,
+        "initial_max_std": 0.2,
+        "initial_min_std": 0.1,
         "ensemble_lr": 0.001,
         "ensemble_epochs": 100,
         "latent_size": 20,
@@ -703,7 +760,7 @@ def cbas_policy(local_dir, cpus, gpus, num_parallel, num_samples):
              num_gpus=gpus,
              temp_dir=os.path.expanduser('~/tmp'))
     tune.run(condition_by_adaptive_sampling, config={
-        "logging_dir": "hopper",
+        "logging_dir": "data",
         "is_discrete": False,
         "task": "HopperController-v0",
         "task_kwargs": {},
