f09ceedb1c395750a6a6f526c59c25ce1099a557
Brandon Trabucco <brandon@btrabucco.com>
2020-09-03 17:16:45 -0700

    add parameters for autofocused CbAS

--- design_baselines/cli.py
+++ design_baselines/cli.py
@@ -849,6 +849,64 @@ def cbas_superconductor(local_dir, cpus, gpus, num_parallel, num_samples):
 
 
 @cli.command()
+@click.option('--local-dir', type=str, default='autofocused-cbas-molecule')
+@click.option('--cpus', type=int, default=24)
+@click.option('--gpus', type=int, default=1)
+@click.option('--num-parallel', type=int, default=1)
+@click.option('--num-samples', type=int, default=1)
+def autofocused_cbas_molecule(local_dir, cpus, gpus, num_parallel, num_samples):
+    """Train a forward model using various regularization methods and
+    solve a model-based optimization problem
+
+    Args:
+
+    local_dir: str
+        the path where model weights and tf events wil be saved
+    cpus: int
+        the number of cpu cores on the host machine to use
+    gpus: int
+        the number of gpu nodes on the host machine to use
+    num_parallel: int
+        the number of processes to run at once
+    num_samples: int
+        the number of samples to take per configuration
+    """
+
+    from design_baselines.autofocused_cbas import autofocused_cbas
+    ray.init(num_cpus=cpus,
+             num_gpus=gpus,
+             temp_dir=os.path.expanduser('~/tmp'))
+    tune.run(autofocused_cbas, config={
+        "logging_dir": "data",
+        "is_discrete": True,
+        "task": "MoleculeActivity600885-v0",
+        "task_kwargs": {},
+        "bootstraps": 5,
+        "val_size": 200,
+        "oracle_batch_size": 128,
+        "vae_batch_size": 32,
+        "hidden_size": 256,
+        "initial_max_std": 1.5,
+        "initial_min_std": 0.5,
+        "oracle_lr": 0.001,
+        "oracle_epochs": 100,
+        "autofocus_epochs": 10,
+        "latent_size": 32,
+        "vae_lr": 0.001,
+        "vae_beta": 5.0,
+        "offline_epochs": 100,
+        "online_batches": 50,
+        "online_epochs": 10,
+        "iterations": 50,
+        "percentile": 80.0,
+        "solver_samples": 128},
+        num_samples=num_samples,
+        local_dir=local_dir,
+        resources_per_trial={'cpu': cpus // num_parallel,
+                             'gpu': gpus / num_parallel - 0.01})
+
+
+@cli.command()
 @click.option('--local-dir', type=str, default='autofocused-cbas-gfp')
 @click.option('--cpus', type=int, default=24)
 @click.option('--gpus', type=int, default=1)
