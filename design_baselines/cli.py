18646ff8531623220d57af8714d76077b62f6486
Brandon Trabucco <brandon@btrabucco.com>
2020-09-06 11:18:32 -0700

    update experiments

--- design_baselines/cli.py
+++ design_baselines/cli.py
@@ -15,6 +15,63 @@ def cli():
 
 
 @cli.command()
+@click.option('--local-dir', type=str, default='conservative-ensemble-ant')
+@click.option('--cpus', type=int, default=24)
+@click.option('--gpus', type=int, default=1)
+@click.option('--num-parallel', type=int, default=1)
+@click.option('--num-samples', type=int, default=1)
+def conservative_ensemble_ant(local_dir, cpus, gpus, num_parallel, num_samples):
+    """Train a forward model using various regularization methods and
+    solve a model-based optimization problem
+
+    Args:
+
+    local_dir: str
+        the path where model weights and tf events wil be saved
+    cpus: int
+        the number of cpu cores on the host machine to use
+    gpus: int
+        the number of gpu nodes on the host machine to use
+    num_parallel: int
+        the number of processes to run at once
+    num_samples: int
+        the number of samples to take per configuration
+    """
+
+    from design_baselines.conservative_ensemble import conservative_ensemble
+    ray.init(num_cpus=cpus,
+             num_gpus=gpus,
+             temp_dir=os.path.expanduser('~/tmp'))
+    tune.run(conservative_ensemble, config={
+        "logging_dir": "data",
+        "task": "AntMorphology-v0",
+        "task_kwargs": {},
+        "is_discrete": False,
+        "noise_std": 0.0,
+        "val_size": 200,
+        "batch_size": 128,
+        "epochs": 100,
+        "activations": [['leaky_relu', 'leaky_relu']],
+        "hidden_size": 2048,
+        "initial_max_std": 0.2,
+        "initial_min_std": 0.1,
+        "forward_model_lr": 0.001,
+        "target_conservative_gap": 0.0,
+        "initial_alpha": 0.0,
+        "alpha_lr": 0.0,
+        "perturbation_lr": 0.00005,
+        "perturbation_steps": 0,
+        "perturbation_backprop": False,
+        "solver_samples": 128,
+        "solver_lr": tune.sample_from(lambda c: c['config']['perturbation_lr']),
+        "solver_steps": 100},
+        num_samples=num_samples,
+        local_dir=local_dir,
+        resources_per_trial={'cpu': cpus // num_parallel,
+                             'gpu': gpus / num_parallel - 0.01})
+
+
+@cli.command()
 @click.option('--local-dir', type=str, default='perturbation-backprop-policy')
 @click.option('--cpus', type=int, default=24)
 @click.option('--gpus', type=int, default=1)
@@ -244,6 +301,122 @@ def conservative_ensemble_superconductor(local_dir, cpus, gpus, num_parallel, nu
 
 
 #############
+
+
+@cli.command()
+@click.option('--local-dir', type=str, default='forward-ensemble-dkitty')
+@click.option('--cpus', type=int, default=24)
+@click.option('--gpus', type=int, default=1)
+@click.option('--num-parallel', type=int, default=1)
+@click.option('--num-samples', type=int, default=1)
+def forward_ensemble_dkitty(local_dir, cpus, gpus, num_parallel, num_samples):
+    """Train a forward model using various regularization methods and
+    solve a model-based optimization problem
+
+    Args:
+
+    local_dir: str
+        the path where model weights and tf events wil be saved
+    cpus: int
+        the number of cpu cores on the host machine to use
+    gpus: int
+        the number of gpu nodes on the host machine to use
+    num_parallel: int
+        the number of processes to run at once
+    num_samples: int
+        the number of samples to take per configuration
+    """
+
+    from design_baselines.forward_ensemble import forward_ensemble
+    ray.init(num_cpus=cpus,
+             num_gpus=gpus,
+             temp_dir=os.path.expanduser('~/tmp'))
+    tune.run(forward_ensemble, config={
+        "logging_dir": "data",
+        "task": "DKittyMorphology-v0",
+        "task_kwargs": {},
+        "is_discrete": False,
+        "bootstraps": 2,
+        "noise_std": 0.0,
+        "val_size": 200,
+        "batch_size": 128,
+        "epochs": 100,
+        "hidden_size": 128,
+        "initial_max_std": 0.2,
+        "initial_min_std": 0.1,
+        "forward_model_lr": 0.001,
+        "solver_samples": 128,
+        "solver_lr": tune.grid_search([0.000750,
+                                       0.000500,
+                                       0.000250,
+                                       0.000100,
+                                       0.000075,
+                                       0.000050,
+                                       0.000025,
+                                       0.000010]),
+        "solver_steps": 500},
+        num_samples=num_samples,
+        local_dir=local_dir,
+        resources_per_trial={'cpu': cpus // num_parallel,
+                             'gpu': gpus / num_parallel - 0.01})
+
+
+@cli.command()
+@click.option('--local-dir', type=str, default='forward-ensemble-ant')
+@click.option('--cpus', type=int, default=24)
+@click.option('--gpus', type=int, default=1)
+@click.option('--num-parallel', type=int, default=1)
+@click.option('--num-samples', type=int, default=1)
+def forward_ensemble_ant(local_dir, cpus, gpus, num_parallel, num_samples):
+    """Train a forward model using various regularization methods and
+    solve a model-based optimization problem
+
+    Args:
+
+    local_dir: str
+        the path where model weights and tf events wil be saved
+    cpus: int
+        the number of cpu cores on the host machine to use
+    gpus: int
+        the number of gpu nodes on the host machine to use
+    num_parallel: int
+        the number of processes to run at once
+    num_samples: int
+        the number of samples to take per configuration
+    """
+
+    from design_baselines.forward_ensemble import forward_ensemble
+    ray.init(num_cpus=cpus,
+             num_gpus=gpus,
+             temp_dir=os.path.expanduser('~/tmp'))
+    tune.run(forward_ensemble, config={
+        "logging_dir": "data",
+        "task": "AntMorphology-v0",
+        "task_kwargs": {},
+        "is_discrete": False,
+        "bootstraps": 2,
+        "noise_std": 0.0,
+        "val_size": 200,
+        "batch_size": 128,
+        "epochs": 100,
+        "hidden_size": 128,
+        "initial_max_std": 0.2,
+        "initial_min_std": 0.1,
+        "forward_model_lr": 0.001,
+        "solver_samples": 128,
+        "solver_lr": tune.grid_search([0.000750,
+                                       0.000500,
+                                       0.000250,
+                                       0.000100,
+                                       0.000075,
+                                       0.000050,
+                                       0.000025,
+                                       0.000010]),
+        "solver_steps": 500},
+        num_samples=num_samples,
+        local_dir=local_dir,
+        resources_per_trial={'cpu': cpus // num_parallel,
+                             'gpu': gpus / num_parallel - 0.01})
 
 
 @cli.command()
@@ -1123,7 +1296,7 @@ def mins_gfp(local_dir, cpus, gpus, num_parallel, num_samples):
         "fully_offline": False,
         "is_discrete": True,
         "is_conv": False,
-        "base_temp": tune.grid_search([0.2, 0.1]),
+        "base_temp": 0.1,
         "keep": 0.9,
         "start_temp": 5.0,
         "final_temp": 1.0,
@@ -1140,7 +1313,7 @@ def mins_gfp(local_dir, cpus, gpus, num_parallel, num_samples):
         "epochs_per_iteration": 20,
         "iterations": 100,
         "exploration_samples": 100,
-        "exploration_rate": 50.,
+        "exploration_rate": 20.,
         "thompson_samples": 100,
         "solver_samples": 100},
         num_samples=num_samples,
