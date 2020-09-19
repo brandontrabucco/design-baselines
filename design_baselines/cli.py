2978ea72405d2f99d0de02dd424f0f074312b3d0
Brandon Trabucco <brandon@btrabucco.com>
2020-09-19 14:09:40 -0700

    adding sweep for conservative score function on dkitty

--- design_baselines/cli.py
+++ design_baselines/cli.py
@@ -45,26 +45,83 @@ def conservative_ensemble_dkitty(local_dir, cpus, gpus, num_parallel, num_sample
     tune.run(conservative_ensemble, config={
         "logging_dir": "data",
         "task": "DKittyMorphology-v0",
-        "task_kwargs": {"split_percentile": 40},
+        "task_kwargs": {"split_percentile": 40, 'num_parallel': 4},
         "is_discrete": False,
         "noise_std": 0.0,
         "val_size": 200,
         "batch_size": 128,
-        "epochs": 100,
+        "epochs": 500,
         "activations": [['leaky_relu', 'leaky_relu']],
         "hidden_size": 2048,
         "initial_max_std": 0.2,
         "initial_min_std": 0.1,
         "forward_model_lr": 0.001,
         "target_conservative_gap": 0.0,
-        "initial_alpha": 0.0,
+        "initial_alpha": .01,
         "alpha_lr": 0.0,
-        "perturbation_lr": 0.00005,
-        "perturbation_steps": 0,
+        "perturbation_lr": 0.001,
+        "perturbation_steps": tune.grid_search([0, 6, 12, 18, 25]),
         "perturbation_backprop": False,
         "solver_samples": 128,
         "solver_lr": tune.sample_from(lambda c: c['config']['perturbation_lr']),
-        "solver_steps": 100},
+        "solver_steps": 500},
+        num_samples=num_samples,
+        local_dir=local_dir,
+        resources_per_trial={'cpu': cpus // num_parallel,
+                             'gpu': gpus / num_parallel - 0.01})
+
+
+@cli.command()
+@click.option('--local-dir', type=str, default='perturbation-backprop-dkitty')
+@click.option('--cpus', type=int, default=24)
+@click.option('--gpus', type=int, default=1)
+@click.option('--num-parallel', type=int, default=1)
+@click.option('--num-samples', type=int, default=1)
+def perturbation_backprop_dkitty(local_dir, cpus, gpus, num_parallel, num_samples):
+    """Train a forward model using various regularization methods and
+    solve a model-based optimization problem
+
+    Args:
+
+    local_dir: str
+        the path where model weights and tf events wil be saved
+    cpus: int
+        the number of cpu cores on the host machine to use
+    gpus: int
+        the number of gpu nodes on the host machine to use
+    num_parallel: int
+        the number of processes to run at once
+    num_samples: int
+        the number of samples to take per configuration
+    """
+
+    from design_baselines.conservative_ensemble import conservative_ensemble
+    ray.init(num_cpus=cpus,
+             num_gpus=gpus,
+             temp_dir=os.path.expanduser('~/tmp'))
+    tune.run(conservative_ensemble, config={
+        "logging_dir": "data",
+        "task": "DKittyMorphology-v0",
+        "task_kwargs": {"split_percentile": 40, 'num_parallel': 4},
+        "is_discrete": False,
+        "noise_std": 0.0,
+        "val_size": 200,
+        "batch_size": 128,
+        "epochs": 500,
+        "activations": [['leaky_relu', 'leaky_relu']],
+        "hidden_size": 2048,
+        "initial_max_std": 0.2,
+        "initial_min_std": 0.1,
+        "forward_model_lr": 0.001,
+        "target_conservative_gap": 0.0,
+        "initial_alpha": .01,
+        "alpha_lr": 0.0,
+        "perturbation_lr": 0.001,
+        "perturbation_steps": tune.grid_search([0, 6, 12, 18, 25]),
+        "perturbation_backprop": True,
+        "solver_samples": 128,
+        "solver_lr": tune.sample_from(lambda c: c['config']['perturbation_lr']),
+        "solver_steps": 500},
         num_samples=num_samples,
         local_dir=local_dir,
         resources_per_trial={'cpu': cpus // num_parallel,
@@ -102,7 +159,7 @@ def conservative_ensemble_ant(local_dir, cpus, gpus, num_parallel, num_samples):
     tune.run(conservative_ensemble, config={
         "logging_dir": "data",
         "task": "AntMorphology-v0",
-        "task_kwargs": {"split_percentile": 20, 'num_parallel': 12},
+        "task_kwargs": {"split_percentile": 20, 'num_parallel': 4},
         "is_discrete": False,
         "noise_std": 0.0,
         "val_size": 200,
