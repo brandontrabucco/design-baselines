5921175eeed476dc0f74584b854dae4255f6edd2
Brandon Trabucco <brandon@btrabucco.com>
2021-03-27 20:41:21 -0700

    adding tfbind

--- design_baselines/online/experiments.py
+++ design_baselines/online/experiments.py
@@ -223,6 +223,9 @@ def superconductor(local_dir, cpus, gpus, num_parallel, num_samples):
                              'gpu': gpus / num_parallel - 0.01})
 
 
+########
+
+
 @cli.command()
 @click.option('--local-dir', type=str, default='online-molecule')
 @click.option('--cpus', type=int, default=24)
@@ -264,7 +267,7 @@ def molecule(local_dir, cpus, gpus, num_parallel, num_samples):
         "negatives_fraction": 1.0,
         "lookahead_steps": 1,
         "lookahead_backprop": True,
-        "evaluate_steps": [i for i in range(0, 2000, 10)],
+        "evaluate_steps": [i for i in range(0, 500, 10)],
         "solver_lr": 0.01,
         "solver_interval": 1,
         "solver_warmup": 50,
@@ -317,12 +320,68 @@ def gfp(local_dir, cpus, gpus, num_parallel, num_samples):
         "negatives_fraction": 1.0,
         "lookahead_steps": 1,
         "lookahead_backprop": True,
-        "evaluate_steps": [i for i in range(0, 2000, 10)],
+        "evaluate_steps": [i for i in range(0, 500, 10)],
         "solver_lr": 0.01,
         "solver_interval": 1,
         "solver_warmup": 50,
         "solver_steps": 1,
         "solver_beta": tune.grid_search([0.0, 0.1, 0.3, 0.7, 0.9, 1.0])},
+        num_samples=num_samples,
+        local_dir=local_dir,
+        resources_per_trial={'cpu': cpus // num_parallel,
+                             'gpu': gpus / num_parallel - 0.01})
+
+
+
+
+
+@cli.command()
+@click.option('--local-dir', type=str, default='online-tfbind8')
+@click.option('--cpus', type=int, default=24)
+@click.option('--gpus', type=int, default=1)
+@click.option('--num-parallel', type=int, default=1)
+@click.option('--num-samples', type=int, default=1)
+def tfbind8(local_dir, cpus, gpus, num_parallel, num_samples):
+    """Evaluate Conservative Score Models on GFP-v0
+    """
+
+    # Final Version
+
+    from design_baselines.online import online
+    ray.init(num_cpus=cpus,
+             num_gpus=gpus,
+             include_dashboard=False,
+             temp_dir=os.path.expanduser(f'~/tmp_{randint(0, 1000000)}'))
+    tune.run(online, config={
+        "logging_dir": "data",
+        "task": "TFBind8-v0",
+        "task_kwargs": {'split_percentile': 40},
+        "is_discrete": True,
+        "constraint_type": "mix",
+        "normalize_ys": True,
+        "normalize_xs": True,
+        "continuous_noise_std": 0.2,
+        "discrete_clip": 0.6,
+        "val_size": 500,
+        "batch_size": 128,
+        "epochs": 500,
+        "activations": ['leaky_relu', 'leaky_relu'],
+        "hidden_size": 512,
+        "initial_max_std": 0.2,
+        "initial_min_std": 0.1,
+        "forward_model_lr": 0.0001,
+        "initial_alpha": 1.0,
+        "alpha_lr": 0.05,
+        "target_conservatism": 0.05,
+        "negatives_fraction": 1.0,
+        "lookahead_steps": 1,
+        "lookahead_backprop": True,
+        "evaluate_steps": [i for i in range(0, 500, 10)],
+        "solver_lr": 0.01,
+        "solver_interval": 1,
+        "solver_warmup": 50,
+        "solver_steps": 1,
+        "solver_beta": 0.0},
         num_samples=num_samples,
         local_dir=local_dir,
         resources_per_trial={'cpu': cpus // num_parallel,
