a03fc9b76a389f80ae8878a99944a5158cab0616
Ubuntu <azureuser@aviral-gpu.wbivlocrbnyerlyws4ppdxnnoc.cx.internal.cloudapp.net>
2021-07-06 09:14:49 +0000

    update experiment scripts

--- scripts/coms-utr.sh
+++ scripts/coms-utr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 NUM_TRIALS_PER_GPU=2
 IFS=, read -ra DEVICES <<< "$CUDA_VISIBLE_DEVICES"
+for OE_LIMIT in 0.05 0.01 0.2 0.5; do
 for DEVICE in "${DEVICES[@]}"; do
     for TRIAL in $(seq $NUM_TRIALS_PER_GPU); do
         CUDA_VISIBLE_DEVICES=$DEVICE coms \
-            --logging-dir ./COMs-UTR-ResNet-v0-$DEVICE-$TRIAL-$RANDOM \
+            --logging-dir ~/coms-utr/coms-utr-$OE_LIMIT/COMs-UTR-ResNet-v0-$DEVICE-$TRIAL-$RANDOM \
             --task UTR-ResNet-v0 \
             --no-task-relabel \
             --normalize-ys \
@@ -31,9 +32,10 @@ for DEVICE in "${DEVICES[@]}"; do
             --forward-model-lr 0.0003 \
             --forward-model-alpha 0.1 \
             --forward-model-alpha-lr 0.01 \
-            --forward-model-overestimation-limit 0.1 \
+            --forward-model-overestimation-limit $OE_LIMIT \
             --forward-model-noise-std 0.0 \
             --forward-model-batch-size 128 \
             --forward-model-val-size 500 \
             --forward-model-epochs 50 \
             --evaluation-samples 128 & done; done; wait
+done
